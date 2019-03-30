3294b3268b6c6073fff188b52d99c8b511b44a62
Anders Perrone <perronea@ohsu.edu>
2019-03-29 18:16:24 -0700

    Update terms and disable signups

--- src/components/Terms.vue
+++ src/components/Terms.vue
@@ -14,16 +14,16 @@ Welcome to the Swipes for Science Brain QC App! We're very excited to have you j
     <p style="text-align: left">
     1.  The data within this application is a part of the ABCD study. Approved users agree that they have a valid ABCD Data Use Agreement both at their site and for their person.
     </p>
-    <p>
+    <p style="text-align: left">
     2.  By signing up for the Swipes for Science application, you agree that you have been given explicit permission to participate in the quality assessment process by your supervisor, home institution, or other principal ABCD entity.
     </p>
-    <p>
+    <p style="text-align: left">
     3.  Data being used within this application is for the explicit purpose of brain imaging quality assessment as defined by the training criteria within the application.
     </p>
-    <p>
+    <p style="text-align: left">
     4.  These data will not be duplicated, photographed/recorded, or otherwise documented or shared externally unless at the permission or request of the Swipes for Science team.
     </p>
-    <p>
+    <p style="text-align: left">
     5.  Data within this application have been stripped of all identifiable information and will not be used in an attempt to establish individual identities from the study.
     </p>
   </div>
